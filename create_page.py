123f2cbc30709aae6fb229dec8f7372c904e57dd
yujitomo <yujitomo@gmail.com>
2025-11-01 18:05:55 +0900

    tmp

--- create_page.py
+++ create_page.py
@@ -1,7 +1,6 @@
 from os import listdir
 import os
 from pathlib import Path
-from typing import Literal
 
 from pydantic import BaseModel
 from jinja2 import Template
@@ -10,35 +9,21 @@ from jinja2 import Template
 class MyAssetData(BaseModel):
     href: str
     view_name: str
-    asset_type: Literal["article", "solution", "note"]
 
 
 MY_ASSETS: list[MyAssetData] = [
-    MyAssetData(href="https://doi.org/10.2996/kmj47304", view_name="Master", asset_type="article"),
-    MyAssetData(href="https://www.ms.u-tokyo.ac.jp/journal/abstract/jms310204.html", view_name="Doctor", asset_type="article"),
+    MyAssetData(href="https://doi.org/10.2996/kmj47304", view_name="Master"),
+    MyAssetData(href="https://www.ms.u-tokyo.ac.jp/journal/abstract/jms310204.html", view_name="Doctor"),
 ]
 
 
 def read_assets() -> list[MyAssetData]:
     pdfs = [os.path.splitext(f)[0] for f in listdir(Path(__file__).parent / "assets") if os.path.splitext(f)[1] == ".pdf"]
-    return [MyAssetData(href=f"https://yujitomo.github.io/assets/{filename}.pdf", view_name=filename, asset_type="note") for filename in pdfs]
+    return [MyAssetData(href=f"https://yujitomo.github.io/assets/{filename}.pdf", view_name=filename) for filename in pdfs]
 
 
 def create_page() -> None:
-
-    articles = []
-    solutions = []
-    notes = []
-    for asset in MY_ASSETS:
-        if asset.asset_type == "article":
-            articles.append(asset)
-        if asset.asset_type == "solution":
-            solutions.append(asset)
-        if asset.asset_type == "note":
-            notes.append(asset)
-
-    contents = {"page_title": "ゆじ 倉庫", "my_assets": MY_ASSETS + read_assets(), "articles": articles, "solutions": solutions, "notes": notes}
-
+    contents = {"page_title": "ゆじ 倉庫", "my_assets": MY_ASSETS + read_assets()}
     with (Path(__file__).parent / "template.html").open("r", encoding="utf-8") as f:
         template = Template(f.read())
     with (Path(__file__).parent / "index.html").open("w", encoding="utf-8") as f:
